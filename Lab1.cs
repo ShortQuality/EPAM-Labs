40682c1995c7d1874a470e780bd29cbac077236d
ShortQuality <61378851+ShortQuality@users.noreply.github.com>
2020-03-14 21:55:05 +0300

    Add files via upload

---
+++ Lab1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+                            //7 variant of tasks.
+namespace ThirdLesson
+{
+    class Lab1
+    {
+        static double Compose(double a, double b)
+        {
+            return a * b; ;
+        }
+
+        static void Enter_of_values(out double a, out double b)
+        {
+            try
+            {
+            string[] arr;
+            arr = Console.ReadLine().Split(' ');
+            a = Convert.ToDouble(arr[0]);
+            b = Convert.ToDouble(arr[1]);
+            }
+            catch
+            {
+                a = b = 0;
+                Console.WriteLine("Incorrect enter");
+            }            
+        }
+
+        
+        static void Main(string[] args)
+        {
+             Enter_of_values(out double a, out double b);
+             Console.WriteLine(Compose(a, b));
+        }
+    }
+}
